ee884467cb314389a68c0d57410618da03a8cf71
steven hunt <st3v3n.hunt@gmail.com>
2016-06-13 08:48:15 +0100

    :dolls: More applications

--- brewcask.sh
+++ brewcask.sh
@@ -3,15 +3,27 @@
 # Get cask https://caskroom.github.io/
 brew tap caskroom/cask
 
-# Install applications
+# Browsers
 brew cask install google-chrome
+brew cask install firefox
+
+# Utils
 brew cask install spectacle
-brew cask install atom
-brew cask install sublime-text
 brew cask install iterm2
-brew cask install slack
 brew cask install google-drive
 brew cask install dropbox
 brew cask install evernote
 brew cask install caffeine
 brew cask install vmware-horizon-client
+
+# Editors
+brew cask install atom
+brew cask install sublime-text
+
+# IM
+brew cask install slack
+brew cask install skype
+
+# Miss
+brew cask install sonos
+brew cask install garmin-express
