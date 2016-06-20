37150cd56fd273a146e231c6561fcf46e1cb054b
steven hunt <st3v3n.hunt@gmail.com>
2016-06-20 21:07:22 +0100

    :interrobang: Install Vundle only if not already installed

--- install-vundle.sh
+++ install-vundle.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+printf "Installing Vundle\n"
+if [[ ! -d ~/.vim/bundle/Vundle.vim/ ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+printf "Installing Vundle Plugins\n"
+vim +PluginInstall +qall
