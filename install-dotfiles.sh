5dd96bdaf0a1277a44b3cd477d450ae392f82d61
steven hunt <st3v3n.hunt@gmail.com>
2017-02-26 12:27:12 +0000

    :abc: Alphabetise for easier reading

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -8,18 +8,18 @@ DOTFILES_DIR="$REPO_DIR/dotfiles"
 
 printf "${GREEN}Starting to symlink...${NC}\n"
 
-ln -nfsv "$DOTFILES_DIR/.bashrc" ~
-ln -nfsv "$DOTFILES_DIR/.bash_profile" ~
-ln -nfsv "$DOTFILES_DIR/.gitconfig" ~
-ln -nfsv "$DOTFILES_DIR/.gitconfig.local" ~
 ln -nfsv "$DOTFILES_DIR/.aliases" ~
 ln -nfsv "$DOTFILES_DIR/.azure-cli" ~
+ln -nfsv "$DOTFILES_DIR/.bash_profile" ~
+ln -nfsv "$DOTFILES_DIR/.bashrc" ~
+ln -nfsv "$DOTFILES_DIR/.gitconfig" ~
+ln -nfsv "$DOTFILES_DIR/.gitconfig.local" ~
 ln -nfsv "$DOTFILES_DIR/.prompt" ~
+ln -nfsv "$DOTFILES_DIR/.tern-project" ~
+ln -nfsv "$DOTFILES_DIR/.tmux.conf" ~
 ln -nfsv "$DOTFILES_DIR/.vimrc" ~
 ln -nfsv "$REPO_DIR/.git-prompt.sh" ~
 ln -nfsv "$REPO_DIR/.git-completion.bash" ~
-ln -nfsv "$DOTFILES_DIR/.tmux.conf" ~
-ln -nfsv "$DOTFILES_DIR/.tern-project" ~
 
 printf "${GREEN}Finished creating symlinks...${NC}\n"
 
