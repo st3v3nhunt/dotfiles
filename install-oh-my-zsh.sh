1af6763709c80123800694fb2072ddba334e5859
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-19 17:51:18 +0100

    :hammer: Use bash shell so shellcheck can run

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
@@ -7,5 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 POWERLEVEL_INSTALL_PATH=$ZSH/custom/themes/powerlevel10k
 if [[ -n $ZSH && ! -d $POWERLEVEL_INSTALL_PATH ]]; then
   echo "powerlevel10k theme will be downloaded..."
-  git clone https://github.com/romkatv/powerlevel10k.git $POWERLEVEL_INSTALL_PATH
+  git clone https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL_INSTALL_PATH"
 fi
