59e90e36636423cf80278c00ff35e3d0f3b0ed46
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-07 14:33:24 +0100

    :see_no_evil: Ensure dir exists and correctly link to file

--- scripts/install-vim-stuff.sh
+++ scripts/install-vim-stuff.sh
@@ -25,4 +25,5 @@ npm install -g eslint_d
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use existing word list for vim spell
-ln -nfsv "$REPO_DIR/vim/en.utf-8.add" ~/.vim/spell
+mkdir ~/.vim/spell
+ln -nfsv "$REPO_DIR/vim/en.utf-8.add" ~/.vim/spell/
