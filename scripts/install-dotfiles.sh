6aa5a786c32f04af8ab132c6546fb043fa2e1a6f
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-05-25 22:49:58 +0100

    :two: Use specific tmux.conf for wsl and macos

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -17,12 +17,19 @@ ln -nfsv "$DOTFILES_DIR/.prompt" ~
 ln -nfsv "$DOTFILES_DIR/.p10k.zsh" ~
 ln -nfsv "$DOTFILES_DIR/.tern-project" ~
 ln -nfsv "$DOTFILES_DIR/.shared-shell-setup" ~
-ln -nfsv "$DOTFILES_DIR/.tmux.conf" ~
 ln -nfsv "$DOTFILES_DIR/.vimrc" ~
 ln -nfsv "$DOTFILES_DIR/.zshrc" ~
-ln -nfsv "$DOTFILES_DIR/.zprofile" ~
 ln -nfsv "$GIT_DIR/.git-prompt.sh" ~
 ln -nfsv "$GIT_DIR/.git-completion.bash" ~
 ln -nfsv "$GIT_DIR/.gitconfig" ~
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ln -nfsv "$DOTFILES_DIR/.macos.tmux.conf" ~/.tmux.conf
+fi
+
+if [[ -n "$WSL_DISTRO_NAME" && "$OSTYPE" == "linux-gnu" ]]; then
+  ln -nfsv "$DOTFILES_DIR/.wsl.tmux.conf" ~/.tmux.conf
+  ln -nfsv "$DOTFILES_DIR/.zprofile" ~
+fi
 
 printf "${GREEN}Finished creating symlinks...${NC}\\n"
