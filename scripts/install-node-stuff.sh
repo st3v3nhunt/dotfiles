0100e3f31fa2f68c27c30bf8ed3ea4c339f0bf01
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-06-23 11:47:02 +0100

    feat: install deno autocomplete for all OSes

--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -9,3 +9,19 @@ npm install -g serverless
 npm install -g snyk
 npm install -g typescript
 npm install -g yarn
+
+echo "Installing Deno autocompletion..."
+if [[ -n "$ZSH" ]]; then
+  mkdir ~/.oh-my-zsh/custom/plugins/deno
+  deno completions zsh > ~/.oh-my-zsh/custom/plugins/deno/_deno
+  # shellcheck disable=1090
+  source ~/.zshrc
+else
+  if [[ -n "$WSL_DISTRO_NAME" ]]; then
+    deno completions bash > /home/linuxbrew/.linuxbrew/etc/bash_completion.d/deno.bash
+  else
+    deno completions bash > /usr/local/etc/bash_completion.d/deno.bash
+  fi
+  # shellcheck disable=1090
+  source ~/.bashrc
+fi
