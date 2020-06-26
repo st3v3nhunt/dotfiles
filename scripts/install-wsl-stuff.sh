47b6b44a98407e681e34785f594b3c3ed06e4c99
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-06-26 19:20:47 +0100

    feat: link windows files from within WSL

--- scripts/install-wsl-stuff.sh
+++ scripts/install-wsl-stuff.sh
@@ -5,6 +5,9 @@ sudo apt-get install build-essential curl file git zsh vim.gtk
 sudo apt-get install apt-transport-https
 sudo apt-get install dotnet-sdk-3.1
 
+echo "If there are problems with installing, checkout the troubleshooting on this page"
+echo "https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-2004#install-the-net-core-sdk"
+
 echo "Installing Deno..."
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
@@ -13,5 +16,13 @@ REPO_DIR="$( cd "$( dirname "$(dirname "${BASH_SOURCE[0]}" )" )" && pwd )"
 echo "Copying 'wsl.conf' into /etc/. Password will be required"
 sudo ln -nfsv "$REPO_DIR/wsl/wsl.conf" /etc/
 
-echo "If there are problems with installing, checkout the troubleshooting on this page"
-echo "https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-2004#install-the-net-core-sdk"
+echo "Linking files in Windows to those in the dotfiles repo on the specific WSL distro"
+WINDOWS_USER=st3v3
+WSL_OS="Ubuntu-20.04"
+WSL_USER=shunt
+
+echo "Linking alacritty.yml"
+cmd.exe /c mklink "C:\\Users\\$WINDOWS_USER\\AppData\\Roaming\\alacritty\\alacritty.yml" "\\\\wsl$\\$WSL_OS\\home\\$WSL_USER\\code\\dotfiles\\wsl\\alacritty.yml"
+
+echo "Linking Windows Terminal settings.json"
+cmd.exe /c mklink "C:\\Users\\$WINDOWS_USER\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json" "\\\\wsl$\\$WSL_OS\\home\\$WSL_USER\\code\\dotfiles\\wsl\\settings.json"
