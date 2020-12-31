f9e0e7e456a9aa7657b090a28ca86a9daba78de7
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-31 14:39:27 +0000

    feat: install wasm-pack and cargo-generate

--- scripts/install-rust-stuff.sh
+++ scripts/install-rust-stuff.sh
@@ -6,6 +6,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "Installing cargo watch..."
 cargo install cargo-watch
 
+echo "Installing cargo generate..."
+cargo install cargo-generate
+
 echo "Install rustup and cargo shell completion for bash..."
 rustup completions bash > "$(brew --prefix)/etc/bash_completion.d/rustup.bash-completion"
 rustup completions bash cargo > "$(brew --prefix)/etc/bash_completion.d/cargo.bash-completion"
---
+++ scripts/install-wasm-stuff.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Installing wasm-pack..."
+curl -sSf https://rustwasm.github.io/wasm-pack/installer/init.sh | sh
