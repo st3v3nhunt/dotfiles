9a2922c3d21bcd3f5bd78e14e7ac7991b2344443
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-01-02 17:43:49 +0000

    feat: install cargo-benchcmp

--- scripts/install-rust-stuff.sh
+++ scripts/install-rust-stuff.sh
@@ -3,11 +3,10 @@
 echo "Installing rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo "Installing cargo watch..."
+echo "Installing some useful cargo based utils..."
 cargo install cargo-watch
-
-echo "Installing cargo generate..."
 cargo install cargo-generate
+cargo install cargo-benchcmp
 
 echo "Install rustup and cargo shell completion for bash..."
 rustup completions bash > "$(brew --prefix)/etc/bash_completion.d/rustup.bash-completion"
