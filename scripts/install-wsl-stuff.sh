b3d95ad61b658a320728c19e1af4d4f7b34e63d0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-01-19 17:56:32 +0000

    refactor(wsl): brew can be used to install vim on wsl

--- scripts/install-wsl-stuff.sh
+++ scripts/install-wsl-stuff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install stuff for linux home brew - https://docs.brew.sh/Homebrew-on-Linux
 sudo apt-get update
-sudo apt-get install build-essential curl file git zsh vim.gtk
+sudo apt-get install build-essential curl file git zsh
 sudo apt-get install apt-transport-https
 sudo apt-get install dotnet-sdk-3.1
 
