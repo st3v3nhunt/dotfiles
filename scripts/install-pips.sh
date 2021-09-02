178ced9ebf6f23cb142d1de8014609e631874540
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-09-02 09:30:56 +0100

    feat: install python2 provider for neovim

--- scripts/install-pips.sh
+++ scripts/install-pips.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Installing pips"
-pip3 install detect-secrets
-pip3 install jupyter
-pip3 install pre-commit
-pip3 install pylint
-pip3 install pytest
-pip3 install pytest-cache
-pip3 install pytest-pep8
-pip3 install pytest-watch
+echo "Installing latest version of pip3"
+python3 -m pip install --upgrade pip
+
+pips=(
+detect-secrets
+jupyter
+pre-commit
+pylint
+pynvim
+pytest
+pytest-cache
+pytest-pep8
+pytest-watch
+)
+
+for pip in "${pips[@]}"; do
+  echo "Installing latest version of $pip"
+  pip3 install --upgrade "$pip"
+done
 
 echo "Installing ansible"
 pip3 install --user ansible
+
 echo "Increase the max number of file handles to fix 'Too many open files' error."
 echo "Password will be required"
 sudo launchctl limit maxfiles unlimited
