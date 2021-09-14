db2ea7ebc29d07c68b4eaa44b1fe8a0c7146e449
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-09-14 12:30:46 +0100

    feat: more dynamically clone repos using gh cli

--- scripts/utils/clone-repos.sh
+++ scripts/utils/clone-repos.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 
-repos=(
-defra/ffc-ce-admin-web
-defra/ffc-ce-payment-orchestrator
-defra/ffc-ce-web
-defra/ffc-demo-acceptance-tests
-defra/ffc-demo-calculation-service
-defra/ffc-demo-claim-service
-defra/ffc-demo-payment-service
-defra/ffc-demo-payment-service-core
-defra/ffc-demo-payment-web
-defra/ffc-demo-web
-defra/ffc-demo-web-acceptance-tests
-defra/ffc-development-guide
-defra/ffc-docker-node
-defra/ffc-git-secret-scanning
-defra/ffc-helm-library
-defra/ffc-helm-repository
-defra/ffc-jenkins-pipeline-library
-defra/ffc-kubernetes-configuration
-defra/ffc-pact-broker
+REPO_PATTERNS=(
+ffc-sfi
 )
 
-pushd ~/code/ || exit
-for repo in "${repos[@]}"; do
-  # use the gh cli
-  gh repo clone "$repo"
+CODE_DIR=~/code
+LIMIT=500 # Limit should be enough to cover all repos in org
+GH_ORG=defra
+
+for PATTERN in "${REPO_PATTERNS[@]}"; do
+  REPOS=$(gh repo list $GH_ORG --limit $LIMIT --json nameWithOwner --jq ".[] | select(.nameWithOwner | contains(\"$PATTERN\")) | .nameWithOwner")
+
+  cd $CODE_DIR || exit
+  for REPO in $REPOS; do
+    echo "Checking repo: $REPO"
+    REPO_DIR=$(echo "$REPO" | cut -d/ -f2)
+    if [ ! -d "$REPO_DIR" ]; then
+      echo "$REPO will be cloned"
+      cd "$CODE_DIR" && gh repo clone "$REPO" && cd - > /dev/null || return
+    else
+      echo "$REPO already exists"
+      cd "$REPO_DIR" && gh repo sync && cd - > /dev/null || return
+    fi
+  done
 done
-popd || exit
