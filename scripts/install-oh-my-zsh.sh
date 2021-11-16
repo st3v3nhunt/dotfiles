d133364016ba5b260b49e4d64730a42f6c9bbca3
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-11-16 17:21:52 +0000

    feat: install spaceship theme for zsh

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -6,9 +6,16 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
+# Install spaceship theme (https://github.com/spaceship-prompt/spaceship-prompt)
+SPACESHIP_INSTALL_PATH=$ZSH_CUSTOM/themes/spaceship-prompt
+if [[ -n $ZSH && ! -d $SPACESHIP_INSTALL_PATH ]]; then
+  echo "spaceship theme will be downloaded..."
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$SPACESHIP_INSTALL_PATH" --depth=1
+  ln -s "$SPACESHIP_INSTALL_PATH/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
 # Install powerlevel10k theme (https://github.com/romkatv/powerlevel10k#installation)
 POWERLEVEL_INSTALL_PATH=$ZSH/custom/themes/powerlevel10k
 if [[ -n $ZSH && ! -d $POWERLEVEL_INSTALL_PATH ]]; then
   echo "powerlevel10k theme will be downloaded..."
-  git clone https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL_INSTALL_PATH"
+  git clone https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL_INSTALL_PATH" --depth=1
 fi
