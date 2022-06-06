52efb2d2cefc62856c6f0d94efe07ec11879a327
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-06 23:30:36 +0100

    feat: beginnings of moving vimscript to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.cmd([[set runtimepath^=~/.vim runtimepath+=~/.vim/after]])
+vim.cmd([[let &packpath=&runtimepath]])
+vim.cmd([[source ~/.vimrc]])
+
+require('settings')
+
+vim.api.nvim_set_option('inccommand','split')                                  -- show split preview of all changes
+-- call v:lua.require'lspconfig'.tsserver.setup({})
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,7 @@
+vim.cmd([[
+let g:loaded_perl_provider=0
+let g:loaded_python_provider=0
+let g:loaded_ruby_provider=0
+let g:python3_host_prog='~/.pyenv/shims/python3'
+let g:node_host_prog='/usr/local/bin/node'
+]])
