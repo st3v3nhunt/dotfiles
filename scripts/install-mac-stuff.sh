5099d8d98e2eede46b7f79bf25926592326ed096
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-18 21:13:34 +0100

    feat: no docker-machine-driver to install

--- scripts/install-mac-stuff.sh
+++ scripts/install-mac-stuff.sh
@@ -55,11 +55,5 @@ done
 # start brew services
 brew services start yabai
 
-# Enable docker-machine-driver to access the hypervisor (hyperkit)
-echo "Update permissions for '$HOMEBREW_PREFIX/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit'..."
-echo "Password will be required..."
-sudo chown root:wheel "$HOMEBREW_PREFIX/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit"
-sudo chmod u+s "$HOMEBREW_PREFIX/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit"
-
 echo "Clean up..."
 brew cleanup
