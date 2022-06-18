4cfba0c5a9eea6e185679d12fd828a92e56ea3e5
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-18 16:50:05 +0100

    feat: add abbreviations to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.cmd([[set runtimepath^=~/.vim runtimepath+=~/.vim/after]])
 vim.cmd([[let &packpath=&runtimepath]])
 vim.cmd([[source ~/.vimrc]])
 
+require('abbrev')
 require('settings')
 
 vim.api.nvim_set_option('inccommand','split')                                  -- show split preview of all changes
---
+++ .config/nvim/lua/abbrev.lua
@@ -0,0 +1,2 @@
+-- abbreviations
+vim.cmd([[iabbrev st3 st3v3nhunt]])
