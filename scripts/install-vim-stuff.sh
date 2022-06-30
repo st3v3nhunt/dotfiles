3b344920667cd02de3af1c2d9b18e69559e01292
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-30 07:46:21 +0100

    feat: remove coc, in favour of LSP

--- scripts/install-vim-stuff.sh
+++ scripts/install-vim-stuff.sh
@@ -17,10 +17,8 @@ NEOVIM_LUA_DIR="$NEOVIM_DIR/lua/"
 mkdir -p ~/"$NEOVIM_DIR"
 ln -nfsv "$REPO_DIR/$NEOVIM_INIT" ~/"$NEOVIM_DIR"
 ln -fsv "$REPO_DIR/$NEOVIM_LUA_DIR" ~/"$NEOVIM_DIR"
-ln -nfsv "$REPO_DIR/vim/coc-settings.json" ~/$NEOVIM_DIR/coc-settings.json
 printf "${GREEN}Finished installing Neovim configuration...${NC}\\n"
 
 # Link ftplugin and spell dirs
 ln -fsv "$REPO_DIR/vim/ftplugin" ~/.vim
 ln -fsv "$REPO_DIR/vim/spell" ~/.vim
-ln -nfsv "$REPO_DIR/vim/coc-settings.json" ~/.vim/coc-settings.json
