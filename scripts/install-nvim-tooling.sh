ac002396b99c7c0e9c1fdea89b3543a247ad299d
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-07-01 22:55:35 +0100

    feat: install eslint_d & use short code

--- scripts/install-nvim-tooling.sh
+++ scripts/install-nvim-tooling.sh
@@ -4,13 +4,13 @@
 
 echo "Install Language Servers"
 # https://github.com/mads-hartmann/bash-language-server
-npm install -g bash-language-server
+npm i -g bash-language-server
 
 # https://github.com/rcjsuen/dockerfile-language-server-nodejs
-npm install -g dockerfile-language-server-nodejs
+npm i -g dockerfile-language-server-nodejs
 
 # https://github.com/hrsh7th/vscode-langservers-extracted
-npm install -g vscode-langservers-extracted
+npm i -g vscode-langservers-extracted
 
 # https://github.com/sumneko/lua-language-server
 brew install lua-language-server
@@ -19,20 +19,21 @@ brew install lua-language-server
 brew install rust-analyzer
 
 # https://github.com/sveltejs/language-tools/tree/master/packages/language-server
-npm install -g svelte-language-server
+npm i -g svelte-language-server
 
 # https://github.com/tailwindlabs/tailwindcss-intellisense
-npm install -g @tailwindcss/language-server
+npm i -g @tailwindcss/language-server
 
 # https://github.com/rust-analyzer/lsp-server
 cargo install --features lsp --locked taplo-cli
 
 # https://github.com/typescript-language-server/typescript-language-server
-npm install -g typescript typescript-language-server
+npm i -g typescript typescript-language-server
 
 # https://github.com/redhat-developer/yaml-language-server
-npm install -g yaml-language-server
+npm i -g yaml-language-server
 
 echo "Install other tools used by Neovim"
 # https://github.com/igorshubovych/markdownlint-cli
 npm i -g markdownlint-cli
+npm i -g eslint_d
