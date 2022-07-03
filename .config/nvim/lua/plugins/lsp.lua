70179d7d4fa92dc5db0ca43f6d516ef18cd03174
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-07-03 18:06:21 +0100

    feat: telemtry setting is default false

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -103,10 +103,6 @@ nvim_lsp.sumneko_lua.setup {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
     },
   },
 }
