ba4f9555b336d62053e66f2edec505398c707d6c
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-07-03 18:01:03 +0100

    feat: config sumneko ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,7 +50,7 @@ g.markdown_fenced_languages = {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local nvim_lsp = require 'lspconfig'
+local nvim_lsp = require('lspconfig')
 nvim_lsp.bashls.setup {
   capabilities = capabilities,
   on_attach = on_attach,
@@ -89,6 +89,26 @@ nvim_lsp.rust_analyzer.setup {
 nvim_lsp.sumneko_lua.setup {
   capabilities = capabilities,
   on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 nvim_lsp.svelte.setup {
   capabilities = capabilities,
