a4f97d7740eebeff3011363aae94a6e4abf9fa70
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-07-20 22:19:41 +0100

    feat: introduce util for easier sharing of functions

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -33,8 +33,7 @@ local on_attach = function(client, bufnr)
   km.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 
   -- Turn off formatting for servers where there is a formatter setup with nullls
-  -- if client.name == "tsserver" or client.name == "sumneko_lua" then
-  if client.name == "tsserver" then
+  if client.name == "tsserver" or client.name == "jsonls" then
     client.resolved_capabilities.document_formatting = false
   end
 end
---
+++ .config/nvim/lua/utils/utils.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+---Checks if `property` exists at the top level in `package.json`.
+---Depends on `jq`.
+--@param utils (table) containing `root_has_file` function
+--@param property (string) name of property to search for
+--@return (boolean)
+function M:propertyExistsInPackageJson(utils, property)
+  local file_exists = utils.root_has_file({ "package.json" })
+  if not file_exists then
+    return false
+  end
+  local handle = io.popen("jq -j '." .. property .. " | length' package.json")
+  if handle == nil then
+    return false
+  end
+  local output = handle:read("*a")
+  handle:close()
+  return tonumber(output, 10) > 0
+end
+
+return M
