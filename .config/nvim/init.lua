ad417d61a0e419755a530a643c4ad50cc3e03374
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-08-24 10:18:37 +0100

    feat: do not load plugins for VS Code context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,11 +5,12 @@ vim.cmd([[source $HOME/.vimrc]])
 if vim.g.vscode then
   print('Loading VS Code settings...')
   vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
+else
+  require('plugins')
 end
 -- Map leader early so it takes affect in other files
 vim.g.mapleader=","
 
-require('plugins')
 require('user')
 
 vim.api.nvim_set_option('inccommand','split')                                  -- show split preview of all changes
