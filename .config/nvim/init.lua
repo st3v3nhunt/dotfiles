87f258bba402277535456a56041a5fd7617cd937
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-08-31 11:49:28 +0100

    feat: map leader as earlier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,15 @@ vim.cmd([[set runtimepath^=$HOME/.vim runtimepath+=$HOME/.vim/after]])
 vim.cmd([[let &packpath=&runtimepath]])
 vim.cmd([[source $HOME/.vimrc]])
 
+-- Map leader early so it takes affect in other files
+vim.g.mapleader=","
+
 if vim.g.vscode then
   print('Loading VS Code settings...')
   vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
 else
   require('plugins')
 end
--- Map leader early so it takes affect in other files
-vim.g.mapleader=","
 
 require('user')
 
