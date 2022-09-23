1918da35a9c31822b586ea7eaf2f5597073f0740
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-09-23 07:58:03 +0100

    feat: move hop to less used key (#394)

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,5 +1,5 @@
 require('hop').setup()
 
 local setKeyMap = vim.keymap.set
-setKeyMap('n', 'f', "<cmd>lua require'hop'.hint_words()<cr>", {})
-setKeyMap('n', 'F', "<cmd>lua require'hop'.hint_words({ current_line_only = true })<cr>", {})
+setKeyMap('n', 's', "<cmd>lua require'hop'.hint_words()<cr>", {})
+setKeyMap('n', 'S', "<cmd>lua require'hop'.hint_words({ current_line_only = true })<cr>", {})
