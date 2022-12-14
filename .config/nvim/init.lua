a5ad17385c3da79491125b49e35e1d6d867d9db3
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-12-14 13:51:28 +0000

    fix: gqq not working when nullls attached

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.cmd([[let &packpath=&runtimepath]])
 vim.cmd([[source $HOME/.vimrc]])
 
 -- Map leader early so it takes affect in other files
-vim.g.mapleader=","
+vim.g.mapleader = ","
 
 if vim.g.vscode then
   print('Loading VS Code settings...')
@@ -14,4 +14,12 @@ end
 
 require('user')
 
-vim.api.nvim_set_option('inccommand','split')                                  -- show split preview of all changes
+-- show split preview of all changes
+vim.api.nvim_set_option('inccommand', 'split')
+-- use internal formatting for bindings like gq.
+-- [source](https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131#issuecomment-1268760653)
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
