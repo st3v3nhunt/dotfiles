aec02ecd44df0c8fbcd52ed3dea427d7739b335e
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2023-10-28 17:24:58 +0000

    feat: install file watcher for ubuntu

--- scripts/install-wsl-stuff.sh
+++ scripts/install-wsl-stuff.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install build-essential curl file git zsh
 sudo apt-get install apt-transport-https
 sudo apt-get install dotnet-sdk-3.1
+sudo apt-get install inotify-tools
 
 echo "If there are problems with installing, checkout the troubleshooting on this page"
 echo "https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-2004#install-the-net-core-sdk"
