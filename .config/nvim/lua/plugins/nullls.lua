0c561dfc5fc2ea58dda58d496c6c663513b1331c
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2024-07-16 15:36:29 +0100

    fix: remove unused variables

--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -1,20 +1,9 @@
 local null_ls = require("null-ls")
-local my_utils = require("utils.utils")
 
-local codeactions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local uses_eslint = function(utils)
-  return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json" })
-  or my_utils.propertyExistsInPackageJson(utils, "eslintConfig")
-end
-
-local uses_standardjs = function(utils)
-  return my_utils.propertyExistsInPackageJson(utils, "standard")
-end
 
 local uses_prettier = function(utils)
   return utils.root_has_file({ ".prettierrc" })
