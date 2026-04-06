e0ee48c48a00e2e3c3c0e5d4c984e63a25945cb5
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2026-04-06 18:32:12 +0100

    feat: remove copilot plugin for neovim

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,11 +1,4 @@
 return {
   { "christoomey/vim-tmux-navigator" },
-  {
-    "github/copilot.vim",
-    config = function()
-      -- Copilot accept with Ctrl-J
-      vim.g.copilot_no_tab_map = true
-    end,
-  },
   { "ludovicchabant/vim-gutentags" },
 }
